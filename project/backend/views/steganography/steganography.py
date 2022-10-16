e7c515fccfa5c95b956d19a94bde8bfd7df94525
R0ryMercury <12345hasek@gmail.com>
2022-10-16 16:46:03 +0300

    created encode view

--- project/backend/views/steganography/steganography.py
+++ project/backend/views/steganography/steganography.py
@@ -1,6 +1,9 @@
 from flask_restx import Resource, Namespace
-from flask import redirect, flash, request
+from flask import redirect, flash, request, render_template
 import os
+from project.backend.ciphres.steganography import image_encode
+
+from project.backend.constants import ALLOWED_EXTENSIONS
 
 stegano_ns = Namespace("steganography")
 
@@ -8,16 +11,22 @@ stegano_ns = Namespace("steganography")
 @stegano_ns.route("/encode/")
 class EncodeView(Resource):
     def get(self):
-        pass
+        return render_template("upload.html")
 
-    # def post(self):
-    #     if "file" not in request.files:
-    #         flash("No file part")
-    #         return redirect(request.url)
-    #     file = request.files["file"]
-    #     if file.filename == "":
-    #         flash("No selected file")
-    #         return redirect(request.url)
-    #     if file and allowed_file(file.filename):
-    #         filename = secure_filename(file.filename)
-    #         file.save(os.path.join(, filename))
+    def post(self):
+        picture = request.files.get("picture")
+        message = request.form.get("content")
+
+        if not picture or not message:
+            flash("Отсутствует картинка или текст")
+            return redirect(request.url)
+        if picture.filename.split(".")[-1] not in ALLOWED_EXTENSIONS:
+            flash("Недопустимый формат картинки")
+            return redirect(request.url)
+
+        try:
+            result = image_encode(picture.filename, message)
+        except FileNotFoundError:
+            flash("Файл не найден")
+            return redirect(request.url)
+        return render_template("uploaded.html", pic=result)
